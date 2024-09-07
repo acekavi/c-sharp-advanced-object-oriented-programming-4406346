5606a50dea7bd3c313708e8b3aa04447f59d698c
Avishka D. Gamage <45445179+acekavi@users.noreply.github.com>
2024-09-07 11:47:34 +0000

    Defining Clases Done

--- Start/Ch1/DefineClasses/Program.cs
+++ Start/Ch1/DefineClasses/Program.cs
@@ -2,6 +2,15 @@
 // Defining and instantiating classes
 
 // TODO: Create some new Rectangle objects with dimensions
+Rectangle rect1 = new Rectangle(10, 20);
+Rectangle sq1 = new Rectangle(25);
 
-
+Console.WriteLine($"Rectangle Area : {rect1.GetArea()}");
+Console.WriteLine($"Square Area : {sq1.GetArea()}");
 // TODO: Change the values of width and height
+
+// rect1.width = 5;
+// sq1.width = 10;
+
+Console.WriteLine($"Rectangle Area : {rect1.GetArea()}");
+Console.WriteLine($"Square Area : {sq1.GetArea()}");
--- Start/Ch1/DefineClasses/shapes.cs
+++ Start/Ch1/DefineClasses/shapes.cs
@@ -2,15 +2,26 @@
 // Defining and instantiating classes
 
 // TODO: Define a class using the "class" keyword
+class Rectangle {
+    // TODO: member variables hold data
+    int width;
+    int height;
 
     // TODO: The constructor accepts parameters used to create the object
-
+    public Rectangle(int width, int height){
+        this.height = height;
+        this.width = width;
+    }
 
     // TODO: For convenience, we can have a constructor that takes one value
     // for squares that have the same side size
 
+    public Rectangle(int side){
+        this.height = this.width = side;
+    }
 
     // TODO: Classes can define methods that return values
-
-
-    // TODO: member variables hold data
+    public int GetArea(){
+        return width * height;
+    }
+}
