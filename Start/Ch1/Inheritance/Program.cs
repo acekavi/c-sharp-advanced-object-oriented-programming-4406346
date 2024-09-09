9e527030ef4ca695406c543c7a50534775ec525e
Avishka D. Gamage <45445179+acekavi@users.noreply.github.com>
2024-09-09 12:02:26 +0000

    Inheritence and access modifiers done

--- Start/Ch1/Inheritance/Program.cs
+++ Start/Ch1/Inheritance/Program.cs
@@ -4,18 +4,28 @@
 // Instantiate some objects
 Circle c = new Circle(10);
 Rectangle r = new Rectangle(10,20);
+Square s = new Square(15);
 
-// TODO: Exercise the ToString() method
+// // TODO: Exercise the ToString() method
+// Console.WriteLine(c.ToString());
+// Console.WriteLine(r.ToString());
 
+// // TODO: Use the "is" operator to test an object type
+// Console.WriteLine($"{c is Rectangle}");
+// Console.WriteLine($"{c is Circle}");
 
-// TODO: Use the "is" operator to test an object type
-
-
-// TODO: Call the GetArea() function on each one
-
+// // TODO: Call the GetArea() function on each one
+// Console.WriteLine($"Area of {c.ToString() + ": " +c.GetArea()}");
+// Console.WriteLine($"Area of {r.ToString() + ": " +r.GetArea()}");
+// Console.WriteLine($"Area of {s.ToString() + ": " +s.GetArea()}");
 
 // TODO: Print the area of each shape
-
+void PrintArea(Shape2D shape){
+    Console.WriteLine($"Area of {shape.ToString() + ": " +shape.GetArea()}");
+}
 
 // TODO: All of the classes derive from Shape2D, so we can treat each one
 // as an instance of the base class. 
+PrintArea(c);
+PrintArea(r);
+PrintArea(s);
