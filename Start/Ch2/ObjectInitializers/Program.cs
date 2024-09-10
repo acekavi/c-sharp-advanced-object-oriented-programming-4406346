d10505e8b348f9a54a02da1193dc8c4f78e36c66
Avishka D. Gamage <45445179+acekavi@users.noreply.github.com>
2024-09-10 15:38:18 +0000

    Instantiating objects

--- Start/Ch2/ObjectInitializers/Program.cs
+++ Start/Ch2/ObjectInitializers/Program.cs
@@ -2,13 +2,43 @@
 // Using object and collection initializers
 
 // TODO: Use the initializer syntax to create new objects
+Dog dog = new Dog {
+    Name = "Luka",
+    Age = 2
+};
 
+Cat cat = new Cat {
+    Name = "Lily",
+    Age = 3,
+    IsDeclawed = true
+};
+
+Console.WriteLine($"{dog.GetType()} {dog.Name} is {dog.Age} years old");
+Console.WriteLine($"{cat.GetType()} {cat.Name} is {cat.Age} years old");
 
 // TODO: Initializers can be used on anonymous types
-
+var pet = new {
+    Name = "Cameo",
+    Age = 5
+};
 
 // TODO: Collections can also be initialized this way
-
+int[] numbers = new int[] { 1,5,6,7,8};
+Console.WriteLine($"{numbers.Length}");
 
 // TODO: Initialize a collection with a set of objects
 
+PetOwner ace = new PetOwner{
+    Name = "Ace",
+    Pets = new List<Pet>{
+        new Dog{
+            Name = "Lilly",
+            Age = 4
+        }, dog, cat
+    }
+};
+
+Console.WriteLine($"{ace}");
+foreach (Pet thisPet in  ace.Pets){
+    Console.WriteLine($"{thisPet.GetType()} {thisPet.Name} is {thisPet.Age} years old");
+}
